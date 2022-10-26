685c222c048779b1ce1729591bc83b1445d4d8db
EvgenyaAnohina <101942009+EvgenyaAnohina@users.noreply.github.com>
2022-10-26 06:19:50 +0300

    parse some elements and create csv

--- lesson_2/main.py
+++ lesson_2/main.py
@@ -1,6 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
+def create_csv(data):
+    with open('data.csv', 'w') as file:
+        writer = csv.writer(file)
+        for _ in data.items():
+            # print(_)
+            writer.writerow(_)
+        # writer.writerow((
+        #         data['name'],
+        #         data['url'],
+        #         data['reviews']
+        #     ))
+
+def refined(s):
+    return int(s.split(' ')[0].strip('(').replace(',', ''))
 
 def get_html(url):
     response = requests.get(url)
@@ -9,7 +24,17 @@ def get_html(url):
 def get_data(html):
     soup = BeautifulSoup(html, 'lxml')
     popular = soup.find_all('section')[3]
-    return popular
+    plugins = popular.find_all('article')
+    for plugin in plugins:
+        name = plugin.find(class_='entry-header').text
+        url = plugin.find('a').get('href')
+        reviews = refined(plugin.find(class_='rating-count').text)
+        data = {
+            'name': name,
+            'url': url,
+            'reviews': reviews
+        }
+        create_csv(data)
 
 def main():
     url = 'https://wordpress.org/plugins/'
